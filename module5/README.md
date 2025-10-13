d4014dba1f6d3fb4724c2a36b799068292f411a1
nayeem-miah <nayeem5113a@gmail.com>
2025-10-13 22:08:42 +0600

    postgres date

---
+++ module5/README.md
@@ -0,0 +1,4 @@
+
+* date and time related worked 
+* date and time formatting
+* SELECT CURRENT_DATE - INTERVAL '1 day';
---
+++ module5/date.sql
@@ -0,0 +1,30 @@
+-- Active: 1760089614932@@127.0.0.1@5432@ph
+-- show timezone;
+
+CREATE Table timeZ (
+    ts TIMESTAMP without time zone,
+ tsz TIMESTAMP with time zone
+);
+
+/* Iam using timestamp with time zone; */
+INSERT INTO timeZ VALUES('2025-10-13 18:37:04.183953+06','2025-10-13 18:37:04.183953+06');
+
+
+SELECT * FROM timeZ;
+SELECT now();
+SELECT CURRENT_DATE;
+SELECT now()::date;
+SELECT now()::time;
+
+SELECT to_char(now(), 'yyyy-mm-dd');
+SELECT to_char(now(), 'yyyy/mm/dd');
+SELECT to_char(now(), 'day');
+
+
+SELECT CURRENT_DATE - INTERVAL '1 day';
+SELECT age(CURRENT_DATE , '2003-11-06');
+
+SELECT * , age(CURRENT_DATE, dob) from students;
+SELECT extract(month from '2004-12-23':: date);
+SELECT TRUE :: BOOLEAN;
+SELECT 'string' :: BOOLEAN;
