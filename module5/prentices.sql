2ed479af89fc6a3739f77f59cd51474fc19df8cc
nayeem-miah <nayeem5113a@gmail.com>
2025-10-15 08:38:26 +0600

    postgres sub query

--- module5/prentices.sql
+++ module5/prentices.sql
@@ -1,4 +1,4 @@
--- Active: 1699467625194@@127.0.0.1@5432@ph@public
+-- Active: 1760089614932@@127.0.0.1@5432@ph@public
 CREATE TABLE employees (
     employee_id SERIAL PRIMARY KEY,
     employee_name VARCHAR(50),
