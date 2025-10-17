b6dd746c0953b2e0aedf5c79d80c6471c4fb295b
nayeem-miah <nayeem5113a@gmail.com>
2025-10-17 12:38:06 +0600

    Show the most recent 2 sightings

--- module7-assainment/assainment.sql
+++ module7-assainment/assainment.sql
@@ -25,4 +25,58 @@ CREATE TABLE sightings (
   notes TEXT
 );
 
-SELECT * from sightings;
+
+-- ? inserting data rangers table
+INSERT INTO rangers (name, region) VALUES
+('Alice Green', 'Northern Hills'),
+('Bob White', 'River Delta'),
+('Carol King', 'Mountain Range');
+
+
+-- ? inserting data species table
+INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status) VALUES
+('Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+('Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
+
+--? inserting data sightings table
+INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VALUES
+(1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
+
+
+--? inserting data rangers table
+INSERT INTO rangers (name, region)
+VALUES ('Derek Fox', 'Coastal Plains');
+
+
+-- ? Count unique species ever sighted.
+SELECT count(DISTINCT species_id)  AS unique_species_count from sightings;
+
+
+-- ? Find all sightings where the location includes "Pass".
+SELECT * From sightings WHERE location ILIKE '%pass%'
+
+
+--? List each ranger's name and their total number of sightings
+
+SELECT r.name, COUNT(s.sighting_id) AS total_sightings
+FROM rangers r 
+LEFT JOIN sightings s  on r.ranger_id = s.ranger_id
+GROUP BY r.name;
+
+-- ? List species that have never been sighted.
+SELECT common_name FROM species
+WHERE species_id not IN(
+    SELECT DISTINCT species_id from sightings
+);
+
+-- ? Show the most recent 2 sightings.
+SELECT species.common_name, sightings.sighting_time, rangers.name from sightings
+JOIN species on sightings.species_id = species.species_id
+JOIN rangers on sightings.ranger_id = rangers.ranger_id
+ORDER BY sightings.sighting_time DESC
+LIMIT 2;
